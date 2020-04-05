87ca320e84eef0a8b1fb0a1008d260fea94a7dff
KuanYuLai <yorklai0523@gmail.com>
2020-04-06 01:12:38 +0800

    edit index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import url from "url";
+
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import pkg from "../package.json";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+/*
+ * Figure out base pathname based on "homepage" property in package.json.
+ */
+const siteURL = pkg.homepage;
+const basename = siteURL ? url.parse(siteURL).pathname : "";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <BrowserRouter basename={basename}>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
